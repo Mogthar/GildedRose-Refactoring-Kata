4dfabbce8f089f6891e3b0d751909e630c119f10
Jirka Kucera <Jirka.Kucera@softwire.com>
2023-07-04 17:33:26 +0100

    introduced unit tests for the original items. Still need to implement the conjured items

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -12,58 +12,127 @@ export class Item {
 
 export class GildedRose {
   items: Array<Item>;
+  specialItems: Array<string>;
 
   constructor(items = [] as Array<Item>) {
+    this.CheckItemInput(items);
     this.items = items;
+    this.specialItems = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros']
   }
 
+  // TODO
+  // conjured items degrade in quality twice as fast 
+
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+    for(let i = 0; i < this.items.length; i++)
+    {
+      // edge cases
+      if(this.specialItems.includes(this.items[i].name))
+      {
+        // handle Aged Brie
+        if(this.items[i].name == 'Aged Brie')
+        {
+          // decrement sellin date and increase quality; CAN BRIE QUALITY GO ABOVE 50?
+          this.items[i].sellIn -= 1;
+          if(this.items[i].quality < 50)
+          {
+            this.items[i].quality += 1;
           }
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
+        
+        // handle tickets
+        if(this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert')
+        {
+          // decrement sellin date and increase quality; CAN BRIE QUALITY GO ABOVE 50?
+          this.items[i].sellIn -= 1;
+          if(this.items[i].sellIn < 0)
+          {
+            this.items[i].quality = 0;
+          }
+          else if(this.items[i].sellIn <= 10 && this.items[i].sellIn > 5)
+          {
+            this.items[i].quality += 2;
+          }
+          else if(this.items[i].sellIn <= 5 && this.items[i].sellIn >= 0)
+          {
+            this.items[i].quality += 3;
+          }
+          else
+          {
+            this.items[i].quality += 1;
+          }
+
+          // cap quality
+          if(this.items[i].quality > 50)
+          {
+            this.items[i].quality = 50;
           }
         }
+        
+        // note that sulfuras doesnt need any handling. It is perceived through time as it should
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+      // normal cases
+      else
+      {
+        // decrement sellin date
+        this.items[i].sellIn -= 1;
+
+        //based on time left, reduce quality either by 1 or by 2
+        if(this.items[i].sellIn >= 0)
+        {
+          this.items[i].quality -= 1;
+        }
+        else
+        {
+          this.items[i].quality -= 2;
+        }
+
+        // cap quality at 0
+        if(this.items[i].quality < 0)
+        {
+          this.items[i].quality = 0;
+        }
+
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
+    }
+    
+    return this.items;
+  }
+
+  CheckItemInput(inputItems){
+    // input quality is less than 50 and greater than 0
+
+    for(let i = 0; i < inputItems.length; i++)
+    {
+      // check sulfuras
+      if(inputItems[i].name == 'Sulfuras, Hand of Ragnaros')
+      {
+        if(inputItems[i].quality != 80)
+        {
+          throw 'Sulfuras has to have a quality of 80';
+        }
+      }
+      
+      // check tickets
+      else if(inputItems[i].name == 'Backstage passes to a TAFKAL80ETC concert')
+      {
+        if(inputItems[i].sellIn < 0 && inputItems[i].quality != 0)
+        {
+          throw 'Tickets are worthless after the concert';
+        }
+      }
+
+      // check the rest
+      else
+      {
+        if(inputItems[i].quality < 0 || inputItems[i].quality > 50)
+        {
+          throw 'An item has to a have a non-negative quality less than or equal to 50';
         }
       }
     }
-
-    return this.items;
   }
 }
+
+
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,98 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
+// general functionality of the inn
 describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
+  });
+
+});
+
+// sulfuras tests
+describe('Sulfuras', () => {
+  it('shouldnt degrade in quality over time', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+
+  it('shouldnt be input with quality other than 80', () => {
+    expect(() => {const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 70)]);}).toThrow();
   });
 });
+
+// brie test
+describe('Brie', () => {
+  it('should increase in quality but be capped at 50', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 3, 49)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it('shouldnt be initialized with wrong quality', () => {
+    expect(() => {const gildedRose = new GildedRose([new Item('Aged Brie', 0, -10)]);}).toThrow();
+  });
+});
+
+// ticket test
+describe('Ticket', () => {
+  it('should increase in quality but be capped at 50', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 35)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(36);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(38);
+
+    for(let i = 0; i < 5; i++)
+    {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].quality).toBe(49);
+
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it('shouldnt be initialized with wrong quality', () => {
+    expect(() => {const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', -1, 5)]);}).toThrow();
+  });
+});
+
+// generic item tests
+describe('Generic item', () => {
+  it('shouldnt be initialized with wrong quality', () => {
+    expect(() => {const gildedRose = new GildedRose([new Item('generic item', 0, 70)]);}).toThrow();
+  });
+
+  it('should reduce sellIn date of a generic item by 1 ', () => {
+    const gildedRose = new GildedRose([new Item('generic item', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+  });
+
+  it('should reduce quality of a generic item by 1 before sellin date', () => {
+    const gildedRose = new GildedRose([new Item('generic item', 6, 8)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(7);
+  });
+
+  it('should reduce quality of a generic item by 2 after sellin date', () => {
+    const gildedRose = new GildedRose([new Item('generic item', -2, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+  });
+
+  it('quality of a generic item is never negative', () => {
+    const gildedRose = new GildedRose([new Item('generic item', -1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+});
+
+
+// 
